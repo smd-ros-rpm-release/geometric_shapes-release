82fdd40a845ab4b8816aa1bc08c3ea2824630016
Ioan Sucan <isucan@willowgarage.com>
2013-05-23 13:10:01 +0300

    Rebase from 'release/groovy/geometric_shapes'

--- src/bodies.cpp
+++ src/bodies.cpp
@@ -41,6 +41,16 @@
 
 extern "C"
 {
+#ifdef GEOMETRIC_SHAPES_HAVE_QHULL_2011
+#include <libqhull/libqhull.h>
+#include <libqhull/mem.h>
+#include <libqhull/qset.h>
+#include <libqhull/geom.h>
+#include <libqhull/merge.h>
+#include <libqhull/poly.h>
+#include <libqhull/io.h>
+#include <libqhull/stat.h>
+#else
 #include <qhull/qhull.h>
 #include <qhull/mem.h>
 #include <qhull/qset.h>
@@ -49,6 +59,7 @@ extern "C"
 #include <qhull/poly.h>
 #include <qhull/io.h>
 #include <qhull/stat.h>
+#endif
 }
 
 #include <boost/math/constants/constants.hpp>
